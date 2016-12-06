7fbb39c4a968751dce18f016fdb684e39c05b94b
Ethan Rogers <erogers6264@gmail.com>
2016-12-05 22:30:21 -0700

    Check that rentals are listed

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -11,6 +11,10 @@ test('should redirect to rentals route.', function(assert) {
 });
 
 test('should list available rentals.', function(assert) {
+  visit('/');
+  andThen(() => {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
 });
 
 test('should link to information about the company.', function(assert) {
